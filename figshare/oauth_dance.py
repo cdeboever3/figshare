157ecee74a1af81016606a9d48c4460660176116
Christopher DeBoever <cdeboever3@gmail.com>
2014-06-20 09:33:46 -0700

    Fixed initial oauth authetication
    Upon first running scripts/figshare at the command line, I was getting
    an error "ValueError: No client verifier has been set." from
    oauth_requests. Referring to
    http://requests-oauthlib.readthedocs.org/en/latest/oauth1_workflow.html,
    I saw that some parts of the initial authentication process were missing
    like getting the request token and authorization. I added them in. I
    changed some of the variable names to match those in the RTD page above.
    It might be worth standardizing the variable names in that function to
    those in the RTD example.

--- figshare/oauth_dance.py
+++ figshare/oauth_dance.py
@@ -46,14 +46,23 @@ def oauth_dance(app_name, consumer_key, consumer_secret, token_filename=None):
     """
     print("Hi there! We're gonna get you all set up to use %s." % app_name)
     auth = OAuth1Session(consumer_key, consumer_secret)
-    access_token = auth.fetch_access_token('http://api.figshare.com/v1/pbl/oauth/request_token')
+    request_token_url = 'http://api.figshare.com/v1/pbl/oauth/request_token'
+    fetch_response = auth.fetch_request_token(request_token_url)
+    resource_owner_key = fetch_response.get('oauth_token')
+    resource_owner_secret = fetch_response.get('oauth_token_secret')
+    
+    base_authorization_url = 'http://api.figshare.com/v1/pbl/oauth/authorize'
 
     print("""
 In the web browser window that opens please choose to Allow
 access. Copy the PIN number that appears on the next page and paste or
 type it here:
 """)
-    oauth_url = auth.authorization_url('http://api.figshare.com/v1/pbl/oauth/authorize')
+    authorize_url = base_authorization_url + '?oauth_token='
+    authorize_url = authorize_url + resource_owner_key
+    print('Please go here and authorize,', authorize_url)
+    verifier = raw_input('Please input the verifier: ')
+    oauth_url = authorize_url
 
     try:
         assert 'DISPLAY' in os.environ
@@ -70,9 +79,15 @@ your PIN:
 
 """ + oauth_url)
     oauth_verifier = _input("Please enter the PIN: ").strip()
-    access_token = auth.fetch_access_token('http://api.figshare.com/v1/pbl/oauth/access_token?oauth_verifier=%s' % oauth_verifier)
-    oauth_token = access_token['oauth_token']
-    oauth_token_secret = access_token['oauth_token_secret']
+    access_token_url = 'http://api.figshare.com/v1/pbl/oauth/access_token'
+    oauth = OAuth1Session(consumer_key, 
+                          client_secret=consumer_secret,
+                          resource_owner_key=resource_owner_key,
+                          resource_owner_secret=resource_owner_secret,
+                          verifier=oauth_verifier)
+    oauth_tokens = oauth.fetch_access_token(access_token_url)
+    oauth_token = oauth_tokens.get('oauth_token')
+    oauth_token_secret = oauth_tokens.get('oauth_token_secret')
 
     if token_filename:
         write_token_file(
